f0436eb318fe5afdd306a0ad8bee1c9707636685
abenezer.m <ebenezermelkamu@gmail.com>
2021-06-15 20:35:43 +0300

    Add local doc store

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const ejs = require("ejs");
 const app = express();
 const env = require("./src/utils/env");
 const router = require("./src/router");
+const { initializeLocalDocStore } = require("./src/router/document");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
@@ -11,6 +12,10 @@ app.use("/", router);
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public")) 
+
+//create a store for local documents (if it is not created)
+initializeLocalDocStore()
+
 
 const port = env.port;
 app.listen(port, console.log(`Server running on port ${port}`));
