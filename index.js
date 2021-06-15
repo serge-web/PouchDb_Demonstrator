c6690f09192b0d04dfd6ad4c5cb7b4f5067c3ead
abenezer.m <ebenezermelkamu@gmail.com>
2021-06-15 05:20:22 +0300

    Remove bodyparser config

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const ejs = require("ejs");
-const bodyParser = require("body-parser") 
 
 const app = express();
-const urlencodedParser = bodyParser.urlencoded({exrended:  false})
-
 const env = require("./src/utils/env");
 const router = require("./src/router");
 
 app.use(express.json());
-app.use("/", urlencodedParser, router);
+app.use(express.urlencoded({extended: false}))
+app.use("/", router); 
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public")) 
